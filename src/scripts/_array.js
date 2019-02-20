e4e987dfa56fb50279b6beadcf9467dbc676f66c
gasseklopper <m-morley@gmx.de>
2019-02-20 22:15:37 +0100

    array testin js and markup and data update

---
+++ src/scripts/_array.js
@@ -0,0 +1,6 @@
+var content = document.querySelectorAll('.js-array-content')
+console.log(content)
+
+content.forEach((elem) =>   {
+console.log(elem)
+})
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,2 @@
 import './_styleguide'
+import './_array'
