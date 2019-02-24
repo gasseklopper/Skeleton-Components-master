57e5818f2a8510f34f313c03a31283217a13ea3b
gasseklopper <m-morley@gmx.de>
2019-02-24 16:39:17 +0100

    added samme height and flex box holy grail

---
+++ src/components/sameHeight/sameHeight.data.js
@@ -0,0 +1,63 @@
+//Array Test variables ----------------------------------------------------------------------------- //
+
+// headers ------------------------------------------------------------------------------------ //
+module.exports = {
+	headers: [
+		{
+			column: 'primary',
+			header: 'primary-header',
+			bodytext: '<p>primary-bodytext -</br></br> All right, kid. But youd better be right about this. All right. Whats your plan? Uh...Threepio, hand me those binders there will you? Okay. Im going to put these on you. Okay. Han, you put these on. Dont worry, Chewie. I think I know what he has in mind. Master Luke, sir!</p>',
+			bodyshort: '<p>primary-bodyshort -</br></br> All right, kid. But youd better be right about this.</p>',
+			subitems:[
+				{
+					name: 'primary-one'
+				},
+				{
+					name: 'primary-two'
+				}
+			]
+		},
+		{
+			column: 'secondary',
+			header: 'secondary-header',
+			bodytext: '<p>secondary-bodytext</p>',
+			bodyshort: '<p>secondary-bodyshort</p>',
+			subitems:[
+				{
+					name: 'secondary-one'
+				},
+				{
+					name: 'secondary-two'
+				}
+			]
+		},
+		{
+			column: 'tertiary',
+			header: 'tertiary-header',
+			bodytext: '<p>tertiary-bodytext</p>',
+			bodyshort: '<p>tertiary-bodyshort</p>',
+			subitems:[
+				{
+					name: 'tertiary-one'
+				},
+				{
+					name: 'tertiary-two'
+				}
+			]
+		},
+		{
+			column: 'quaternary',
+			header: 'quaternary-header',
+			bodytext: '<p>quaternary-bodytext</p>',
+			bodyshort: '<p>quaternary-bodyshort</p>',
+			subitems:[
+				{
+					name: 'quaternary-one'
+				},
+				{
+					name: 'quaternary-two'
+				}
+			]
+		}
+	]
+}
