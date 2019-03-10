6993a74e55bc14d19c1154a9f14ab34d1c80a42f
gasseklopper <m-morley@gmx.de>
2019-03-10 12:48:11 +0100

    added content 01

---
+++ src/components/content_01/content_01.data.js
@@ -0,0 +1,67 @@
+//Array Test variables ----------------------------------------------------------------------------- //
+
+// headers ------------------------------------------------------------------------------------ //
+module.exports = {
+	content: [
+		{
+			column: 'primary',
+			header: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia',
+			bodytext: '',
+			bodyshort: '<p>primary-bodyshort -</br></br> All right, kid. But youd better be right about this.</p>',
+			subitems:[
+				{
+					background_image: 'primary-one',
+					image:{
+						src: 'https://via.placeholder.com/772x355.png/09f/fff',
+					}
+				}
+			]
+		},
+		{
+			column: 'secondary',
+			header: '',
+			bodytext: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut ero labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco poriti laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in uienply voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat norin proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>',
+			quote: '<p><i>Sed do eiusmod tempor incididunt ut ero labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco poriti laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in uienply voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat norin proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</i></p>',
+			bodyshort: '<p>secondary-bodyshort</p>',
+			subitems:[
+				{
+					background_image: 'primary-one',
+					image:{
+						src: 'https://via.placeholder.com/772x355.png/09f/fff',
+					}
+				}
+			]
+		}
+	],
+	content2: [
+		{
+			column: 'primary',
+			header: 'primary-header',
+			bodytext: '<p>primary-bodytext -</br></br> All right, kid. But youd better be right about this. All right. Whats your plan? Uh...Threepio, hand me those binders there will you? Okay. Im going to put these on you. Okay. Han, you put these on. Dont worry, Chewie. I think I know what he has in mind. Master Luke, sir!</p>',
+			bodyshort: '<p>primary-bodyshort -</br></br> All right, kid. But youd better be right about this.</p>',
+			subitems:[
+				{
+					background_image: 'primary-one',
+					image:{
+						src: 'https://via.placeholder.com/772x355.png/09f/fff',
+					}
+
+				}
+			]
+		},
+		{
+			column: 'secondary',
+			header: 'secondary-header',
+			bodytext: '<p>secondary-bodytext</p>',
+			bodyshort: '<p>secondary-bodyshort</p>',
+			subitems:[
+				{
+					background_image: 'primary-one',
+					image:{
+						src: 'https://via.placeholder.com/772x355.png/09f/fff',
+					}
+				}
+			]
+		}
+	]
+}
