9adfb3f28b1f2b73de6861bcb855e7be0b776fdb
gasseklopper <m-morley@gmx.de>
2019-03-23 13:16:47 +0100

    added landing page

---
+++ src/components/1landing/1landing.data.js
@@ -0,0 +1,77 @@
+//Array Test variables ----------------------------------------------------------------------------- //
+
+// headers ------------------------------------------------------------------------------------ //
+module.exports = {
+	content: [
+		{
+			column: 'header',
+			header: 'Header - header',
+			bodytext: '<p>primary-bodytext -</br></br> All right, kid. But youd better be right about this. All right. Whats your plan? Uh...Threepio, hand me those binders there will you? Okay. Now</p>',
+			bodyshort: '<p>primary-bodyshort</p>',
+			subitems:[
+				{
+					name: 'primary-one'
+				},
+				{
+					name: 'primary-two'
+				}
+			]
+		},
+		{
+			column: 'sidebar1',
+			header: 'Sidebar 1 - header',
+			bodytext: '<p>secondary-bodytext</p>',
+			bodyshort: '<p>secondary-bodyshort</p>',
+			subitems:[
+				{
+					name: 'secondary-one'
+				},
+				{
+					name: 'secondary-two'
+				}
+			]
+		},
+		{
+			column: 'sidebar2',
+			header: 'Sidebar 2 - header',
+			bodytext: '<p>tertiary-bodytext</p>',
+			bodyshort: '<p>tertiary-bodyshort</p>',
+			subitems:[
+				{
+					name: 'tertiary-one'
+				},
+				{
+					name: 'tertiary-two'
+				}
+			]
+		},
+		{
+			column: 'main-content',
+			header: 'Main Content - header',
+			bodytext: '<p>quaternary-bodytext</p>',
+			bodyshort: '<p>quaternary-bodyshort</p>',
+			subitems:[
+				{
+					name: 'quaternary-one'
+				},
+				{
+					name: 'quaternary-two'
+				}
+			]
+		},
+		{
+			column: 'footer',
+			header: 'FSooter - header',
+			bodytext: '<p>quaternary-bodytext</p>',
+			bodyshort: '<p>quaternary-bodyshort</p>',
+			subitems:[
+				{
+					name: 'quaternary-one'
+				},
+				{
+					name: 'quaternary-two'
+				}
+			]
+		}
+	]
+}
