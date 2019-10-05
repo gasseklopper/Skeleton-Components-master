6675af1ada0673af21775aaee129ff5d0e60e4a2
gasseklopper <m-morley@gmx.de>
2019-10-06 00:07:55 +0200

    anime layered

---
+++ src/components/anime_layered/anime_layered.data.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "animals": [{
+    "type": "dog",
+    "sound": "woof"
+  }, {
+    "type": "cattttt",
+    "sound": "meow"
+  }, {
+    "type": "cow",
+    "sound": "moo"
+  }]
+}
