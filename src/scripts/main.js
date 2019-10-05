27382b3d31f74be7544e66ad58fe089e5f218746
gasseklopper <m-morley@gmx.de>
2019-10-06 00:10:45 +0200

    lax and animate

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,94 @@
+import lax from 'lax.js'
+
+
 import './_styleguide'
+import './_anime_test'
+import './_anime_staggering'
+import './_anime_color_changer'
+import './_anime_layered'
+import './_anime_sphere'
 import './_array'
+import './_plugin_lax'
+
+
+// anime({
+//   targets: '.stagger-visualizer',
+//   translateX: 250,
+//   rotate: '1turn',
+//   backgroundColor: '#FFF',
+//   duration: 800
+// });
+
+
+// function fitElementToParent(el, padding) {
+//   var timeout = null;
+//   function resize() {
+//     if (timeout) clearTimeout(timeout);
+//     anime.set(el, {scale: 1});
+//     var pad = padding || 0;
+//     var parentEl = el.parentNode;
+//     var elOffsetWidth = el.offsetWidth - pad;
+//     var parentOffsetWidth = parentEl.offsetWidth;
+//     var ratio = parentOffsetWidth / elOffsetWidth;
+//     timeout = setTimeout(anime.set(el, {scale: ratio}), 10);
+//   }
+//   resize();
+//   window.addEventListener('resize', resize);
+// }
+
+// var easingsAnimation = (function() {
+
+//   var easingVisualizerEl = document.querySelector('.easing-visualizer');
+//   var barsWrapperEl = easingVisualizerEl.querySelector('.bars-wrapper');
+//   var dotsWrapperEl = easingVisualizerEl.querySelector('.dots-wrapper');
+//   var barsFragment = document.createDocumentFragment();
+//   var dotsFragment = document.createDocumentFragment();
+//   var numberOfBars = 91;
+//   var duration = 450;
+//   var animation;
+
+//   fitElementToParent(easingVisualizerEl, 0);
+
+//   for (var i = 0; i < numberOfBars; i++) {
+//     var barEl = document.createElement('div');
+//     var dotEl = document.createElement('div');
+//     barEl.classList.add('bar');
+//     dotEl.classList.add('dot');
+//     dotEl.classList.add('color-red');
+//     barsFragment.appendChild(barEl);
+//     dotsFragment.appendChild(dotEl);
+//   }
+
+//   barsWrapperEl.appendChild(barsFragment);
+//   dotsWrapperEl.appendChild(dotsFragment);
+
+//   function play() {
+
+//     var easings = [];
+//     for (let ease in anime.penner) easings.push(ease);
+//     easings.push('steps('+anime.random(5, 20)+')');
+//     easings.push('steps('+anime.random(5, 20)+')');
+//     easings.push('cubicBezier(0.545, 0.475, 0.145, 1)');
+//     var ease = easings[anime.random(0, easings.length - 1)];
+
+//     animation = anime.timeline({
+//       duration: duration,
+//       easing: ease,
+//       complete: play
+//     })
+//     .add({
+//       targets: '.easing-visualizer .bar',
+//       scaleY: anime.stagger([1, 44], {easing: ease, from: 'center', direction: 'reverse'}),
+//       delay: anime.stagger(7, {from: 'center'})
+//     })
+//     .add({
+//       targets: '.easing-visualizer .dot',
+//       translateY: anime.stagger(['-160px', '160px'], {easing: ease, from: 'last'}),
+//       delay: anime.stagger(7, {from: 'center'})
+//     }, 0);
+
+//   }
+
+//   play();
+
+// })();
