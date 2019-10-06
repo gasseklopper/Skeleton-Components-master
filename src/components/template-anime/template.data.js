bdb972e8028a4c23a2eebe7211ff5d71fef304aa
gasseklopper <m-morley@gmx.de>
2019-10-06 11:26:31 +0200

    added grid and template

---
+++ src/components/template-anime/template.data.js
@@ -0,0 +1,5 @@
+module.exports = {
+	content_01: require('../content_01/content_01.data'),
+	content_02: require('../content_02/content_02.data'),
+	sameHeight: require('../sameHeight/sameHeight.data'),
+}
